37f94546fbe666d000853ff6d48445b6d04fc2cc
DavinderSingh-ds <davindersingh00743@gmail.com>
2022-07-22 22:09:03 +0530

    Armstrong Nmber

---
+++ armstrong.java
@@ -0,0 +1,26 @@
+class armstrong{
+    public static void main(String[] args) {
+        int num = 153,temp=num;
+        int rem = 0,sum = 0;
+        int count = 0;
+        while(temp>0)
+        {
+            temp = temp/10;
+            count ++;
+        }
+        temp = num;
+        // System.out.println(count);
+        while (num>0) {
+            rem = num%10;
+            num = num/10;
+            sum = sum + (int) Math.pow(rem, count);
+        }
+        System.out.println(temp);
+        System.out.println(sum);
+        if (temp==sum) {
+            System.out.println("armstrong number");
+        } else {
+            System.out.println("not a armstrong number");
+        }
+    }
+}
