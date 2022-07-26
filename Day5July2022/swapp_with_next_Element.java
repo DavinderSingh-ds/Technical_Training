8841b0b50fd74774128cc1edb7a38d86c1b2310b
DavinderSingh-ds <davindersingh00743@gmail.com>
2022-07-27 00:05:34 +0530

    Swap_next-with_Previous

--- Day5July2022/swapp_with_next_Element.java
+++ Day5July2022/swapp_with_next_Element.java
@@ -1,9 +1,9 @@
 public class swapp_with_next_Element {
     public static void main(String[] args) {
-        int a[] = {1,2,3,4,5};
+        int a[] = {1,2,3,4,5,6};
         int temp = 0;
         for (int i = 0; i < a.length; i = i+2) {
-            if (i != 4) {
+            if (i != a.length-1) {
                 temp = a[i];
                 a[i] =a[i+1];
                 a[i+1] = temp;
@@ -12,5 +12,12 @@ public class swapp_with_next_Element {
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
         }
+
+
+        // for (int i = 0; i < a.length-1; i = i+2) {
+        //         temp = a[i];
+        //         a[i] =a[i+1];
+        //         a[i+1] = temp;
+        // }
     }
 }
